d723a4627c95ad48c932169a4db4df458f0d182f
swezinlinn <swezinlinn1@gmail.com>
2020-09-10 12:56:38 +0630

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_bookmarks/model/bookmark.dart';
@@ -10,9 +12,36 @@ void main() {
 class MyBookmarkApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: BookMarkPage());
+
+    return MaterialApp(
+      title: 'Splash Screen',
+        home: MyMainPage(),
+    );
   }
 }
 
 
+class  MyMainPage extends StatefulWidget{
+  @override
+  _MyMainPageState createState() => _MyMainPageState();
+}
 
+class _MyMainPageState extends State<MyMainPage> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) => BookMarkPage()
+            )
+        )
+    );    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+        child:FlutterLogo(size:MediaQuery.of(context).size.height)
+    );
+  }
+}
